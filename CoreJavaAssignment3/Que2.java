ab0042f150b0ef3fa7940b3697e96832e9a9bb4c
Ujjwal Sanjay Dhake <161634172+ujjwal10122000@users.noreply.github.com>
2024-03-14 23:59:08 +0530

    Add files via upload

---
+++ CoreJavaAssignment3/Que2.java
@@ -0,0 +1,9 @@
+public class Que2 {
+    public static void main(String[] args) {
+        double i=10;
+        int j=(int) i;
+        System.out.println(i);
+        System.out.println(j);
+    }
+    
+}
---
+++ CoreJavaAssignment3/Que3.java
@@ -0,0 +1,15 @@
+public class Que3 {
+    public static void main(String[] args) {
+        int i=10;
+        float f =12.2f;
+        double d = 13.123;
+        int result1=(int)(i+f+d);
+        float result2=(float)(i+f+d);
+        double result3=(double)(i+f+d);
+        System.out.println("result store in int :-" + result1);
+        System.out.println("result store in float :-" + result2);
+        System.out.println("result store in double :-" + result3);
+        
+    }
+    
+}
