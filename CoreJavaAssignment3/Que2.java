283a37a9013eb5415d159fa1d591395d5213c312
Ujjwal Sanjay Dhake <161634172+ujjwal10122000@users.noreply.github.com>
2024-03-16 18:36:34 +0530

    Delete CoreJavaAssignment3 directory

--- CoreJavaAssignment3/Que2.java
+++
@@ -1,9 +0,0 @@
-public class Que2 {
-    public static void main(String[] args) {
-        double i=10;
-        int j=(int) i;
-        System.out.println(i);
-        System.out.println(j);
-    }
-    
-}
